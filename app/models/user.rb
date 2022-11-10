f6432c93d4791a3de979917a22b238bf743bd0e1
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-11-10 22:24:03 +0530

    buyms show page style

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :buyms
+  has_many :buyms, dependent: :destroy
 end
